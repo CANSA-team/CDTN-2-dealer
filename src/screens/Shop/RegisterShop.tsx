efe255958488f5c949d30f2511bfdc9580d60034
anhsonxui <anhsonxui@gmail.com>
2021-10-28 08:52:28 +0700

    upload giao dien dang ky shop lan 2

--- src/screens/Shop/RegisterShop.tsx
+++ src/screens/Shop/RegisterShop.tsx
@@ -1,11 +1,10 @@
 import React, { memo, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Image, Text, StyleSheet } from 'react-native';
 import Background from '../../components/Background';
 import Header from '../../components/Header';
 import TextInput from '../../components/TextInput';
-import CheckTemp from '../../components/CheckTemp';
-
-
+import Button from '../../components/Button';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 
 
@@ -16,6 +15,9 @@ export default function RegisterShop() {
 
     return (
         <Background>
+            <TouchableOpacity style={styles.container}>
+                <Image source={require('../../../assets/arrow_back.png')} />
+            </TouchableOpacity>
             <Header>Create Account Dealer</Header>
 
             <TextInput
@@ -26,12 +28,23 @@ export default function RegisterShop() {
                 label="Mô tả Shop"
                 returnKeyType="next"
             />
+            <Button mode="contained" style={styles.button}>
+                Đăng Ký
+            </Button>
         </Background>
 
     );
 };
 
 const styles = StyleSheet.create({
-
+    container: {
+        position: 'absolute',
+        top: 10 + getStatusBarHeight(),
+        left: 10,
+    },
+    image: {
+        width: 24,
+        height: 24,
+    },
 });
 
