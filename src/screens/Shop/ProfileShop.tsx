2bb5d2ad00d423dd28dc801df3bb6813ce2bc76a
anhsonxui <anhsonxui@gmail.com>
2021-10-28 14:57:26 +0700

    up giao dien lan 3: hoan thanh 60% ( giao dien chua dep, chua redux) gom 3 giao dien

---
+++ src/screens/Shop/ProfileShop.tsx
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, Platform, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
+import { Accessory, Avatar, Button } from 'react-native-elements';
+import * as ImagePicker from 'expo-image-picker';
+
+import HeaderTitle from '../../components/HeaderTitle';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import { useNavigation } from '../../utils/useNavigation';
+import axios from 'axios';
+import { cansa } from '../../consts/Selector';
+
+let user_temp = {
+    "id": 1,
+    "phone": "0968241064",
+    "name": "anh",
+    "birthday": "1999-09-28T17:00:00.000Z"
+}
+
+class UserProfile {
+    id?: number;
+    phone?: string;
+    name?: string;
+    birthday?: Date;
+    user_id?: number;
+    user_key?: null;
+    user_name?: string;
+    user_avatar?: string;
+    user_status?: number;
+    user_last_update?: number;
+}
+
+export default function ProfileShop(props: any) {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [image, setImage] = useState('https://i.ibb.co/hYjK44F/anise-aroma-art-bazaar-277253.jpg');
+    const [isLoadingChangePassword, setIsLoadingChangePassword] = useState<boolean>(true);
+
+
+
+    return (
+        <View style={styles.container}>
+            {!isLoading ?
+                (<View style={styles.container}>
+                    <ActivityIndicator size="large" color="#00ff00" />
+                </View>) : (
+                    <View>
+                        <View>
+                            <HeaderTitle title={'PROFILE'} />
+                            <View style={styles.header}>
+                                <TouchableOpacity>
+                                    <MaterialIcons name="arrow-back" size={35} color="white" onPress={() => {return;}} />
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => {return;}}>
+                                    <Feather name="edit" color="white" size={35} />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+
+                        <View style={styles.viewAvatar}>
+                            <Avatar
+                                containerStyle={{ marginBottom: 20 }}
+                                rounded
+                                size={200}
+                                source={{
+                                    uri: image,
+                                }} >
+                            </Avatar>
+                        </View>
+
+                        <View style={styles.viewTxt}>
+                            <View style={styles.txtContainer}>
+                                <Text style={styles.txtTitle}>User full name: </Text>
+                            </View>
+
+                            <View style={styles.txtContainer}>
+                                <Text style={styles.txtTitle}>User nick name:</Text>
+                            </View>
+
+                            <View style={styles.txtContainer}>
+                                <Text style={styles.txtTitle}>User phone: </Text>
+                            </View>
+
+                            <View style={styles.txtContainer}>
+                                <Text style={styles.txtTitle}>User birthday: </Text>
+                            </View>
+
+                            <View style={styles.resetPassContainer}>
+                                {
+                                    isLoadingChangePassword &&
+                                    <TouchableOpacity style={styles.touchReset}
+                                        onPress={() => {
+                                            // setIsLoadingChangePassword(false);
+                                            // let email = getParam('email');
+                                            // axios.get(`${cansa[1]}/api/user/forgot/password/${email}`).then((res) => {
+                                            //     Alert.alert('Thông Báo', res.data.message);
+                                            //     navigate('OTPscreen', { email: email })
+                                            // })
+                                            return;
+                                        }}>
+                                        <Text style={{ fontSize: 20, color: '#555' }}>Đổi mật khẩu</Text>
+                                        <MaterialIcons name="arrow-right-alt" size={35} color="#555" />
+                                    </TouchableOpacity>
+                                }
+
+                            </View>
+                        </View>
+                    </View>)}
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#E5E5E5',
+    },
+    viewAvatar: {
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+        marginHorizontal: 5
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
+        position: 'absolute',
+        top: 33,
+        left: 5,
+        right: 0,
+        zIndex: 2
+    },
+    viewTxt: {
+        marginTop: 3,
+        backgroundColor: '#fff',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+        marginHorizontal: 5
+    },
+    txtContainer: {
+        flexDirection: 'row',
+        margin: 15,
+        marginHorizontal: 10
+    },
+    txtTitle: {
+        fontSize: 20,
+        color: '#111'
+    },
+    resetPassContainer: {
+        borderTopColor: '#ddd',
+        borderTopWidth: 2,
+        margin: 10,
+        marginHorizontal: 10
+    },
+    touchReset: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 10
+    }
+
+});
--- src/screens/Shop/RegisterShop.tsx
+++ src/screens/Shop/RegisterShop.tsx
@@ -4,14 +4,25 @@ import Background from '../../components/Background';
 import Header from '../../components/Header';
 import TextInput from '../../components/TextInput';
 import Button from '../../components/Button';
+import { Avatar, Accessory } from 'react-native-elements';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-
-
-
-
-
+import CircleCheckBox, { LABEL_POSITION } from 'react-native-circle-checkbox';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function RegisterShop() {
+    const [image, setImage] = useState('../../../assets/arrow_back.png');
+    let getImg = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [3, 3],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
 
     return (
         <Background>
@@ -28,9 +39,33 @@ export default function RegisterShop() {
                 label="Mô tả Shop"
                 returnKeyType="next"
             />
+
+            <Avatar
+                containerStyle={{ marginBottom: 20 }}
+                rounded
+                size={100}
+                source={{
+                    uri: image,
+                }}
+                onPress={getImg}
+            >
+                <Accessory style={{
+                    borderWidth: 2,
+                    borderColor: "#444",
+                    backgroundColor: '#000',
+                }} size={50}></Accessory>
+            </Avatar>
+
+            <CircleCheckBox
+                checked={true}
+                onToggle={(checked: any) => console.log('My state is: ', checked)}
+                labelPosition={LABEL_POSITION.RIGHT}
+                label="Đồng Ý Điều Khoản Dịch Vụ"
+            />
             <Button mode="contained" style={styles.button}>
                 Đăng Ký
             </Button>
+
         </Background>
 
     );
@@ -46,5 +81,9 @@ const styles = StyleSheet.create({
         width: 24,
         height: 24,
     },
+    button: {
+        flex: 1,
+    }
+
 });
 
