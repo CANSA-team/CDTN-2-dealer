b169d37a0ecbb3de66394fe78ace66a2826df71f
anhsonxui <anhsonxui@gmail.com>
2021-11-06 10:31:25 +0700

    update UI registershop

--- src/screens/Shop/RegisterShop.tsx
+++ src/screens/Shop/RegisterShop.tsx
@@ -1,12 +1,8 @@
 import React, { memo, useState, useEffect } from 'react';
-import { View, TouchableOpacity, Image, Text, StyleSheet, Alert } from 'react-native';
-import Background from '../../components/Background';
-import Header from '../../components/Header';
-import TextInput from '../../components/TextInput';
-import Button from '../../components/Button';
-import { Avatar, Accessory } from 'react-native-elements';
+import { View, TouchableOpacity, Image, Text, StyleSheet, Alert, CheckBox } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { Avatar, Accessory, Input } from 'react-native-elements';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import CircleCheckBox, { LABEL_POSITION } from 'react-native-circle-checkbox';
 import * as ImagePicker from 'expo-image-picker';
 import {
     shopNameValidator,
@@ -17,14 +13,16 @@ import { State, UserStage, checkLogin, logout, login, ImageId, UserModel, regist
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '../../utils/useNavigation';
 import { saveImage } from '../../consts/Selector';
-
+import HeaderTitle from '../../components/HeaderTitle';
+import COLORS from '../../consts/Colors';
 
 
 export default function RegisterShop() {
-    const [shop_name, setShop_name] = useState({ value: '', error: '' });
-    const [shop_description, setShop_description] = useState({ value: '', error: '' });
+    const [isSelected, setSelection] = useState(false);
+    const [shop_name, setShop_name] = useState('');
+    const [shop_description, setShop_description] = useState('');
 
-    const [image, setImage] = useState({ value: '../../../assets/arrow_back.png', error: '' });
+    const [image, setImage] = useState('../../../assets/arrow_back.png');
     const userState: UserStage = useSelector((state: State) => state.userReducer);
     const { check, status, userInfor }: { check: boolean, status: string, userInfor: UserModel } = userState;
     const { navigate } = useNavigation();
@@ -69,26 +67,25 @@ export default function RegisterShop() {
     }
 
 
-    const _onSignUpPressed = () => {
-        const shop_nameError = shopNameValidator(shop_name.value);
-        const shop_descriptionError = shopDescriptionValidator(shop_description.value);
+    const onPress = () => {
+        const shop_nameError = shopNameValidator(shop_name);
+        const shop_descriptionError = shopDescriptionValidator(shop_description);
 
-        if (shop_nameError || shop_descriptionError || image.value == '../../../assets/arrow_back.png') {
-            setShop_name({ ...shop_name, error: shop_nameError });
-            setShop_description({ ...shop_description, error: shop_descriptionError });
+        if (shop_nameError || shop_descriptionError || image == '../../../assets/arrow_back.png') {
+
 
             return;
         } else {
             console.log(shop_name, shop_description, image);
             let _avatar: ImageId = { id: 0 };
             const avatar_img = {
-                uri: image.value,
+                uri: image,
                 name: 'userProfile.jpg',
                 type: 'image/jpg'
             }
             const saveAvt: Promise<void> = saveImage(avatar_img, _avatar);
             Promise.all([saveAvt]).then(() => {
-                dispatch(registerShop(shop_name.value, shop_description.value, userInfor.user_id, _avatar.id));
+                dispatch(registerShop(shop_name, shop_description, userInfor.user_id, _avatar.id));
             })
         }
 
@@ -103,87 +100,174 @@ export default function RegisterShop() {
         });
 
         if (!result.cancelled) {
-            setImage({ value: result.uri, error: '' });
+            setImage(result.uri);
         }
     };
     return (
-        <>
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <HeaderTitle title={'Đăng Ký Bán Hàng'} />
+                    <View style={styles.header}>
+                        <TouchableOpacity>
+                            <MaterialIcons name="arrow-back" size={35} color="white" onPress={() => { return; }} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-            <Background>
+                <View style={styles.viewAvatar}>
+                    <Avatar
+                        containerStyle={{ marginBottom: 20 }}
+                        rounded
+                        size={200}
+                        source={{
+                            uri: image,
+                        }}
+                        onPress={getImg}
+                    >
+                        <Accessory style={{
+                            borderWidth: 2,
+                            borderColor: "#444",
+                            backgroundColor: COLORS.primary
+                        }} size={50}></Accessory>
+                    </Avatar>
+                </View>
 
-                <TouchableOpacity style={styles.container} onPress={() => _logout()}>
-                    <Image source={require('../../../assets/arrow_back.png')} />
-                </TouchableOpacity>
+                <View style={styles.viewTxt}>
+                    <Input
+                        value={shop_name}
+                        label="Tên Shop"
+                        onChangeText={setShop_name}
+                    />
 
+                    <Input
+                        value={shop_description}
+                        label="Mô tả"
+                        onChangeText={setShop_description}
+                    />
 
-                <Header>Tạo Tài Khoản Bán Hàng</Header>
-                <Avatar
-                    containerStyle={{ marginBottom: 20 }}
-                    rounded
-                    size={100}
-                    source={{
-                        uri: image.value,
-                    }}
-                    onPress={getImg}
-                >
-                    <Accessory style={{
-                        borderWidth: 2,
-                        borderColor: "#444",
-                        backgroundColor: '#000',
-                    }} size={50} tvParallaxProperties={undefined}></Accessory>
-                </Avatar>
+                    <View style={styles.containerC}>
+                        <View style={styles.checkboxContainer}>
+                            <CheckBox
+                                value={isSelected}
+                                onValueChange={setSelection}
+                                style={styles.checkbox}
+                            />
+                            <Text style={styles.label}>Đồng Ý Điều Khoản Sử Dụng? {isSelected ? "👍" : "👎"}</Text>
+                        </View>
+                    </View>
 
-                <TextInput
-                    label="Tên shop"
-                    returnKeyType="next"
-                    value={shop_name.value}
-                    onChangeText={text => setShop_name({ value: text, error: '' })}
-                    error={!!shop_name.error}
-                    errorText={shop_name.error}
-                />
-                <TextInput
-                    label="Mô tả Shop"
-                    returnKeyType="next"
-                    value={shop_description.value}
-                    onChangeText={text => setShop_description({ value: text, error: '' })}
-                    error={!!shop_description.error}
-                    errorText={shop_description.error}
-                    autoCapitalize="none"
-                />
-
-                <CircleCheckBox
-                    checked={false}
-                    onToggle={(checked: any) => console.log('My state is: ', checked)}
-                    labelPosition={LABEL_POSITION.RIGHT}
-                    label="Đồng Ý Điều Khoản Dịch Vụ"
-                />
-                <TouchableOpacity>
-                    <Button mode="contained" onPress={_onSignUpPressed} style={styles.button}>
-                        Đăng Ký
-                    </Button>
-                </TouchableOpacity>
-            </Background>
-        </>
-
+                    <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 25, marginBottom: 8 }}>
+                        <TouchableOpacity onPress={() => {
+                            { return; }
+                        }}>
+                            <Text style={styles.btnBuy}>Lưu</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    txtError: {
-        color: '#f86161'
-    },
+
+    containerC: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      checkboxContainer: {
+        flexDirection: "row",
+        marginBottom: 20,
+      },
+      checkbox: {
+        alignSelf: "center",
+      },
+      label: {
+        margin: 8,
+      },
+
+
+    
     container: {
+        flex: 1,
+        backgroundColor: '#E5E5E5',
+    },
+    viewAvatar: {
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+        marginHorizontal: 5
+    },
+    btnBuy: {
+        backgroundColor: '#00FF7F',
+        padding: 7,
+        width: 150,
+        borderRadius: 20,
+        marginBottom: 10,
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: '700',
+        color: '#222'
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
         position: 'absolute',
-        top: 10 + getStatusBarHeight(),
-        left: 1,
+        top: 33,
+        left: 5,
+        right: 0,
+        zIndex: 2
     },
-    image: {
-        width: 24,
-        height: 24,
+    viewTxt: {
+        paddingTop: 5,
+        marginTop: 3,
+        backgroundColor: '#fff',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+        marginHorizontal: 5
     },
-    button: {
-        marginTop: 24,
+    txtContainer: {
+        flexDirection: 'row',
+        marginHorizontal: 10,
+        alignItems: 'center'
     },
+    txtTitle: {
+        fontSize: 20,
+        color: '#111',
+        marginRight: 20,
+    },
+    resetPassContainer: {
+        borderTopColor: '#ddd',
+        borderTopWidth: 2,
+        margin: 10,
+        marginHorizontal: 10
+    },
+    touchReset: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 10
+    }
 
 });
 
