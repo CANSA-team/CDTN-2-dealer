ae24d5e72226f7fa6fb0fda9e97f902851d7cfe9
Pham Hoang Anh <hoaganh34k@gmail.com>
2021-12-08 19:05:41 +0700

    8/12/2021 Upload anh

--- src/screens/Chat/ListChat.tsx
+++ src/screens/Chat/ListChat.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Text, FlatList, TouchableOpacity, View, StyleSheet } from 'react-native'
+import { Text, FlatList, TouchableOpacity, View, StyleSheet, RefreshControl } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 import { useNavigation } from '../../utils/useNavigation';
 import HeaderTitle from '../../components/HeaderTitle';
@@ -20,6 +20,7 @@ export default function ListChat(props: any) {
     const myID = 'shop_' + info.shop_id;
     const chatState: ChatStage = useSelector((state: State) => state.chatReducer);
     const { isChat }: { isChat?: boolean } = chatState;
+    const [isLoadingChat, setIsLoadingChat]: any = useState(true)
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -48,7 +49,6 @@ export default function ListChat(props: any) {
                     let id = (myID != data[0].user_to) ? data[0].user_to : data[0].user_from
                     socket.emit('onlineStatus', data[0].user_to)
                     let a = await axios.get(`https://103.207.38.200/api/user/get/user/by/${id.split('user_')[1]}`)
-
                     tempData.push({
                         id: '_' + Math.random().toString(36).substr(2, 16),
                         title: a.data.data.user_name,
@@ -83,10 +83,11 @@ export default function ListChat(props: any) {
                     let id = '_' + Math.random().toString(36).substr(2, 16)
                     let id_user = (myID != element[0].user_to) ? element[0].user_to : element[0].user_from
                     let a = await axios.get(`https://103.207.38.200/api/user/get/user/by/${id_user.split('user_')[1]}`)
+
                     tempData.push({
                         id: id,
                         title: a.data.data.user_name,
-                        img: a.data.data.shop_avatar,
+                        img: a.data.data.user_avatar,
                         id_user: (myID != element[0].user_to) ? element[0].user_to : element[0].user_from,
                         newStatusMess: (element[0].isWatched == 1) ? false : true,
                         statusOnline: false,
@@ -98,10 +99,10 @@ export default function ListChat(props: any) {
                 let id = (myID != data[0].user_to) ? data[0].user_to : data[0].user_from
                 socket.emit('onlineStatus', data[0].user_to)
                 let a = await axios.get(`https://103.207.38.200/api/user/get/user/by/${id.split('user_')[1]}`)
-
                 tempData.push({
                     id: '_' + Math.random().toString(36).substr(2, 16),
                     title: a.data.data.user_name,
+                    img:a.data.data.user_avatar,
                     id_user: (myID != data[0].user_to) ? data[0].user_to : data[0].user_from,
                     newStatusMess: (data[0].isWatched == 1) ? false : true,
                     statusOnline: false,
@@ -111,10 +112,53 @@ export default function ListChat(props: any) {
             }
             let temp = tempData.sort((a: any, b: any) => new Date(b.CreateDate).getTime() - new Date(a.CreateDate).getTime())
             setData(temp)
+            setIsLoadingChat(false)
         });
 
     }, [])
 
+    const onRefeshing = () => {
+        setIsLoadingChat(true)
+        socket.emit('roomList', myID)
+        socket.on("roomList", async function (data) {
+            let tempData: any = [];
+            if (data.length !== 1) {
+                await Promise.all(data.map(async (element: any) => {
+                    socket.emit('onlineStatus', element[0].user_to)
+                    let id = '_' + Math.random().toString(36).substr(2, 16)
+                    let id_user = (myID != element[0].user_to) ? element[0].user_to : element[0].user_from
+                    let a = await axios.get(`https://103.207.38.200/api/user/get/user/by/${id_user.split('user_')[1]}`)
+                    tempData.push({
+                        id: id,
+                        title: a.data.data.user_name,
+                        img: a.data.data.user_avatar,
+                        id_user: (myID != element[0].user_to) ? element[0].user_to : element[0].user_from,
+                        newStatusMess: (element[0].isWatched == 1) ? false : true,
+                        statusOnline: false,
+                        CreateDate: element[0].CreateDate,
+                        text: element[0].message,
+                    })
+                }));
+            } else {
+                let id = (myID != data[0].user_to) ? data[0].user_to : data[0].user_from
+                socket.emit('onlineStatus', data[0].user_to)
+                let a = await axios.get(`https://103.207.38.200/api/user/get/user/by/${id.split('user_')[1]}`)
+                tempData.push({
+                    id: '_' + Math.random().toString(36).substr(2, 16),
+                    title: a.data.data.user_name,
+                    img: a.data.data.user_avatar,
+                    id_user: (myID != data[0].user_to) ? data[0].user_to : data[0].user_from,
+                    newStatusMess: (data[0].isWatched == 1) ? false : true,
+                    statusOnline: false,
+                    CreateDate: data[0].CreateDate,
+                    text: data[0].message,
+                })
+            }
+            let temp = tempData.sort((a: any, b: any) => new Date(b.CreateDate).getTime() - new Date(a.CreateDate).getTime())
+            setData(temp)
+            setIsLoadingChat(false)
+        });
+    }
 
     const renderItem = ({ item }: any) => {
         return (
@@ -123,7 +167,7 @@ export default function ListChat(props: any) {
                     <Avatar
                         rounded
                         source={{
-                            uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+                            uri: (item.img !== undefined) ? item.img : 'https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-5&_nc_sid=7206a8&_nc_ohc=NwzaeU5vaKcAX-JkmHJ&_nc_ht=scontent.fsgn2-4.fna&oh=221ebd3067741a5aa6944cbf4be0ad2d&oe=61D540F8',
                         }}
                         size="large"
                     />
@@ -143,6 +187,7 @@ export default function ListChat(props: any) {
             </TouchableOpacity>
         );
     };
+    
     return (
         <SafeAreaView style={styles.container}>
             <HeaderTitle title={'Liên hệ'} />
@@ -151,6 +196,12 @@ export default function ListChat(props: any) {
                 data={data}
                 renderItem={renderItem}
                 keyExtractor={(item: any) => item.id}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={isLoadingChat}
+                        onRefresh={() => { onRefeshing() }}
+                    />
+                }
             />
         </SafeAreaView>
     );
