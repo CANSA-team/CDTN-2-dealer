04e4233b82a5be3253d5d17fabbf23898034fff6
NatswarChuan <81142858+NatswarChuan@users.noreply.github.com>
2022-08-17 01:08:59 +0700

    .

--- src/consts/Selector.ts
+++ src/consts/Selector.ts
@@ -76,10 +76,11 @@ export function updateImage(img: any, id_avatar: number, obj: ImageId) {
 
 
 export const cansa =
-    ["\x68\x74\x74\x70\x3A\x2F\x2F\x31\x30\x33\x2E\x32\x30\x37\x2E\x33\x38\x2E\x32\x30\x30\x3A\x33\x30\x30\x32",
-        "https://103.207.38.200:443", "http://103.207.38.200:3102"];
+    ["http://20.92.229.217:333",
+        "https://20.92.229.217/:443"];
+        
+export const chatSever = 'https://20.92.229.217:4320';
 
-export const chatSever = 'https://103.207.38.200:4320';
 
 
 export function vnd(n: number | string) {
